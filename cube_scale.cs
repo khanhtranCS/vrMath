4ea9eaab722a429a60c79d4e167efe8ff339eb8f
Khanh Duc Tran <ktran035@uw.edu>
2016-10-16 00:56:39 -0700

    implemented dynamic equation to show up in enviroment

--- cube_scale.cs
+++ cube_scale.cs
@@ -1,8 +1,11 @@
 using UnityEngine;
 using System.Collections;
 
-public class object_scale : MonoBehaviour {
 
+public class object_scale : MonoBehaviour {
+	public static float x;
+	public static float y;
+	public static float z;
 	// Use this for initialization
 	void Start () {
 
@@ -10,6 +13,9 @@ public class object_scale : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		x = transform.localScale.x;
+		y = transform.localScale.y;
+		z = transform.localScale.z;
 		if (Input.GetAxis ("Mouse ScrollWheel") > 0) {
 			transform.localScale += new Vector3 (0.1F, 0.1F, 0.1F);
 		};
@@ -19,4 +25,5 @@ public class object_scale : MonoBehaviour {
 			}
 		}
 	}
+
 }
--- equation_sim.cs
+++ equation_sim.cs
@@ -1,18 +1,20 @@
 using UnityEngine;
 using System.Collections;
 
-public class equation_sim : MonoBehaviour {
 
-	// Use this for initialization
+public class equation_sim : MonoBehaviour {
+	//object_scale curr_cube;
+	TextMesh dyn_text;
 	void Start () {
-	
+		
 	}
 	
-	// Update is called once per frame.
+	// Update is called once per frame
 	void Update () {
-		object_scale cube_obj = GetComponent(object_scale);
-		Vector3 cube_dim = cube_obj.getDimension();
-		Text txt = gameObject.GetComponent<Text>();
-		txt.text = cube_dim.x + "" + cube_dim.y + "" + cube_dim.z;
+		//curr_cube = GetComponent<object_scale>();
+		//Vector3 dim = curr_cube.getDimension();
+		Debug.Log(object_scale.x);
+		GetComponent<TextMesh>().text ="x: " + object_scale.x + " y: " + object_scale.y + "z " + object_scale.z;
+
 	}
 }
